27cc615750f0543a8dcd5b34e6daf2a8e964cad3
jasminaramim <jasminaramim2005@gmail.com>
2025-06-04 21:07:34 +0600

    CTa section done

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -361,7 +361,7 @@
 //     </div>
 //   );
 // };
-
+// code from jasmin
 // export default Home;
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
